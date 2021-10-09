1a1419a46f4e2cb983efa3347ed9c20ee53d2646
Rajni Dua <rajni.dua14@gmail.com>
2021-10-09 22:31:30 +1030

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerSchema", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
